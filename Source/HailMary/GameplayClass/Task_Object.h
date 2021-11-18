44d156ad895ea2f01c9dc9125c853604f4b58ae0
Kogane <77961470+ImKogane@users.noreply.github.com>
2021-11-18 14:44:59 +0100

    Add spawn task core system

---
+++ Source/HailMary/GameplayClass/Task_Object.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HailMary/GameplayClass/InteractionBase/InteractibleElement.h"
+#include "Task_Object.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAILMARY_API ATask_Object : public AInteractibleElement
+{
+	GENERATED_BODY()
+	
+};
