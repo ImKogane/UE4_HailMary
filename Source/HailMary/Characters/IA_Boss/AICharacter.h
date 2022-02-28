05e1214929d2dc7aab7e38631f7c421d56949484
Kogane <heughebaert.dev@gmail.com>
2022-02-28 16:55:36 +0100

    Mod. Update stuff

--- Source/HailMary/Characters/IA_Boss/AICharacter.h
+++ Source/HailMary/Characters/IA_Boss/AICharacter.h
@@ -23,6 +23,8 @@ public:
 	/*The Component which is used for the "seeing" sense of the AI*/
 	UPROPERTY(VisibleAnywhere,Category="AI")
 	class UPawnSensingComponent* PawnSensingComp;
+
+	UPROPERTY(EditDefaultsOnly, Category="Sounds")
 	
 	// Sets default values for this character's properties
 	AAICharacter();
