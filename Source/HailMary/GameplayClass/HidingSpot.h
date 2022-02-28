f7f7dcc47f853577e8bdbaf6d05e571711530b39
Sathariel <leochabert@free.fr>
2022-02-28 08:47:40 +0100

    Add, Hiding Spot class

---
+++ Source/HailMary/GameplayClass/HidingSpot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionBase/InteractibleElement.h"
+#include "HidingSpot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAILMARY_API AHidingSpot : public AInteractibleElement
+{
+	GENERATED_BODY()
+	
+};
