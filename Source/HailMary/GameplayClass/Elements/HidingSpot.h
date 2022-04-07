d4055ce2c17d0194369d168e5bfe7dfac445c392
Sathariel <leochabert@free.fr>
2022-04-07 14:31:19 +0200

    Fix, Gameover when players are in differents doors

--- Source/HailMary/GameplayClass/Elements/HidingSpot.h
+++ Source/HailMary/GameplayClass/Elements/HidingSpot.h
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/TargetPoint.h"
@@ -47,7 +44,4 @@ class HAILMARY_API AHidingSpot : public AInteractibleElement
 			virtual void OnBoxOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 			virtual void OnBoxOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,int32 OtherBodyIndex) override;
 		#pragma endregion
-
-	
-	private:
 };
