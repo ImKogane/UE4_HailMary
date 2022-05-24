92d654896b24dc2f54e42fd69a182b2627742f4e
Kogane <heughebaert.dev@gmail.com>
2022-05-24 18:12:04 +0200

    Add, ReversibleProps class

---
+++ Source/HailMary/GameplayClass/Elements/ReversibleProps.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../InteractionBase/InteractibleElement.h"
+#include "ReversibleProps.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAILMARY_API AReversibleProps : public AInteractibleElement
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Elements details")
+	bool IsReverse;
+	
+};
