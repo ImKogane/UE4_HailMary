52b0d9ca20231b27e1b1578020eacd682d853b58
Kogane <heughebaert.dev@gmail.com>
2022-05-25 17:04:17 +0200

    Add, Reversible props

--- Source/HailMary/GameplayClass/Elements/ReversibleProps.cpp
+++ Source/HailMary/GameplayClass/Elements/ReversibleProps.cpp
@@ -5,15 +5,77 @@
 
 #include "HailMary/Characters/StudentCharacter/StudentCharacter.h"
 
+AReversibleProps::AReversibleProps()
+{
+	CollisionBox->SetupAttachment(BaseComponent);
+}
+
+void AReversibleProps::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(CurveFloat)
+	{
+		FOnTimelineFloat TimeLineProgress;
+		TimeLineProgress.BindUFunction(this, FName("TimeLineProgress"));
+
+		CurveTimeLine.AddInterpFloat(CurveFloat, TimeLineProgress);
+		CurveTimeLine.SetLooping(false);
+
+		StartRot = EndRot = GetActorRotation();
+		EndRot.Roll += ZOffset;
+
+		
+	}
+	
+}
+
 void AReversibleProps::Interaction(AActor* Character)
 {
 	Super::Interaction(Character);
+
+	if(!IsFalling)
+	{
+		AStudentCharacter* Player = Cast<AStudentCharacter>(Character);
+
+		if(Player == nullptr) { return; }
+
+		IsFalling = true;
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Falling ..."));
+		CurveTimeLine.PlayFromStart();
+	}
 	
-	AStudentCharacter* Player = Cast<AStudentCharacter>(Character);
-
-	if(Player == nullptr) { return; }
-
-	IsFalling = true;
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Falling ..."));	
 }
+
+void AReversibleProps::FallEffect()
+{
+	Destroy(true);
+}
+
+
+void AReversibleProps::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CurveTimeLine.TickTimeline(DeltaSeconds);
+	
+	if(!CurveTimeLine.IsPlaying() && !IsReverse && IsFalling)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("A terre."));
+		IsReverse = true;
+		IsFalling = false;
+		
+		FTimerHandle UnusedHandle;
+		GetWorldTimerManager().SetTimer(
+			UnusedHandle, this, &AReversibleProps::FallEffect, 10.0f, false);
+	}
+}
+
+void AReversibleProps::TimeLineProgress(float Value)
+{
+	FRotator NewRot = FMath::Lerp(StartRot, EndRot, Value);
+	SetActorRotation(NewRot);
+}
+
+
 
--- Source/HailMary/GameplayClass/Elements/ReversibleProps.h
+++ Source/HailMary/GameplayClass/Elements/ReversibleProps.h
@@ -4,23 +4,62 @@
 
 #include "CoreMinimal.h"
 #include "../InteractionBase/InteractibleElement.h"
+#include "Components/TimelineComponent.h"
 #include "ReversibleProps.generated.h"
 
 /**
  * 
  */
+
+class UCurveFloat;
+
 UCLASS()
 class HAILMARY_API AReversibleProps : public AInteractibleElement
 {
 	GENERATED_BODY()
 
+	AReversibleProps();
+	
 protected:
+
+	
+	
+	virtual void BeginPlay() override;
+	
 	UPROPERTY(VisibleAnywhere, Category = "Elements details")
 	bool IsReverse;
 	
 	UPROPERTY(VisibleAnywhere, Category = "Elements details")
 	bool IsFalling;
-	
+
+	//Timeline
 	virtual void Interaction(AActor* Character) override;
+
+	UFUNCTION()
+	void FallEffect();
+
+	FTimeline CurveTimeLine;
+
+	UPROPERTY(EditAnywhere, Category="Timeline")
+	UCurveFloat* CurveFloat;
+	
+	FTimerHandle TimerHandle;
+
+	float DELTATIME;
+
+	UPROPERTY()
+	FRotator StartRot;
+
+	UPROPERTY()
+	FRotator EndRot;
+
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	float ZOffset;
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+	
+	UFUNCTION()
+	void TimeLineProgress(float Value);
 	
 };
