57cbeb9e006696e489d763694fdfd995d1263c85
Kogane <heughebaert.dev@gmail.com>
2022-06-01 16:19:42 +0200

    Add, Child perk class

---
+++ Source/HailMary/Characters/Perks/perk00_PerkComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perk_BaseComponent.h"
+#include "perk00_PerkComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAILMARY_API Uperk00_PerkComponent : public UPerk_BaseComponent
+{
+	GENERATED_BODY()
+	
+};
